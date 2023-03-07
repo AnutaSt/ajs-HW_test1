1bfc33f0868d1fa48ec4d9c8e829ee31511b7d81
AnnaSt <anutabatuk@gmail.com>
2023-03-07 13:02:05 +0800

    new version

---
+++ src/js/http.js
@@ -0,0 +1,9 @@
+export function httpGet(url) {
+  throw new Error(url);
+}
+
+// Демо-реализация функции fetchData (модуль http):
+// eslint-disable-next-line no-unused-vars
+export default function fetchData(url) {
+  throw new Error('Mock this!');
+}
---
+++ src/js/user.js
@@ -0,0 +1,16 @@
+import fetchData, { httpGet } from './http';
+
+export function loadUser(id) {
+  const data = httpGet(`http://server:8080/users/${id}`);
+  return JSON.parse(data);
+}
+
+export default function getLevel(userId) {
+  const response = fetchData(`https://server/user/${userId}`);
+
+  if (response.status === 'ok') {
+    return `Ваш текущий уровень: ${response.level}`;
+  }
+
+  return 'Информация об уровне временно недоступна';
+}
